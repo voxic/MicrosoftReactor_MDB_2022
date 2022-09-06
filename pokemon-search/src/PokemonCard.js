26619254cb0a8a4fb31d413b047ad226d951efcf
Emil Nildersen <emil@gr8c.se>
2022-09-06 21:04:16 +0200

    initial

---
+++ pokemon-search/src/PokemonCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';
+
+export default function PokemonCard( { name, sprite, species } ) {
+  return (
+    <Card sx={{ minWidth: 100 }}>
+      <CardContent>
+        <Typography align="center" sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Pokemon
+        </Typography>
+        <Typography align="center" variant="h5" component="div">
+        <img src={sprite} />
+        </Typography>
+        
+        <Typography align="center" variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography align="center" sx={{ mb: 1.5 }} color="text.secondary">
+          {species}
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
